54360c73e1f8c14c2d0f6daa126b47cee50e0f81
ilyashe <ilya.shebanoff@gmail.com>
2025-04-08 19:27:09 +0300

    with wrapper

--- utils/utils.py
+++ utils/utils.py
@@ -1,70 +1,69 @@
 import json
 import logging
-
-import requests
-from allure_commons._allure import step
-from allure_commons.types import AttachmentType
 import allure
+from allure_commons.types import AttachmentType
+from allure_commons._allure import step
 
-def demoshop_api_request(url, method='POST', *args, **kwargs):
-    with step('API Request'):
-        result = requests.request(url=url, method=method, *args, **kwargs)
 
-        log_to_allure(result)
-        log_to_console(result)
-        return result
+def log_api_call(func):
+    def wrapper(*args, **kwargs):
+        with step('API Request'):
+            result = func(*args, **kwargs)
 
-def log_to_allure(result):
-    allure.attach(
-        body=f'Method: {result.request.method}\nURL: {result.request.url}',
-        name='Request Info',
-        attachment_type=AttachmentType.TEXT,
-        extension='txt'
-    )
-    allure.attach(
-        body=result.request.body,
-        name='Request Body',
-        attachment_type=AttachmentType.TEXT,
-        extension='txt'
-    )
+            # Request info
+            allure.attach(
+                body=f'Method: {result.request.method}\nURL: {result.request.url}',
+                name='Request Info',
+                attachment_type=AttachmentType.TEXT
+            )
 
-    allure.attach(
-        body=f'Status code: {result.status_code}',
-        name='Response status code',
-        attachment_type=AttachmentType.TEXT,
-        extension='txt'
-    )
+            allure.attach(
+                body=result.request.body,
+                name='Request Body',
+                attachment_type=AttachmentType.TEXT
+            )
 
-    allure.attach(
-        body=json.dumps(result.json(), indent=4, ensure_ascii=True),
-        name='Response',
-        attachment_type=AttachmentType.JSON,
-        extension='json'
-    )
+            # Response
+            allure.attach(
+                body=f'Status code: {result.status_code}',
+                name='Response status code',
+                attachment_type=AttachmentType.TEXT
+            )
 
-    cookies_dict = {cookie.name: cookie.value for cookie in result.cookies}
-    allure.attach(
-        body=json.dumps(cookies_dict, indent=4),
-        name='Cookies',
-        attachment_type=AttachmentType.TEXT,
-        extension='json'
-    )
+            try:
+                response_body = json.dumps(result.json(), indent=4, ensure_ascii=True)
+                allure.attach(
+                    body=response_body,
+                    name='Response',
+                    attachment_type=AttachmentType.JSON
+                )
+            except Exception:
+                pass  # не JSON
 
-def log_to_console(result):
-    log_message = f'''
-            ==== HTTP Request ====
-            Method: {result.request.method}
-            URL: {result.request.url}
-            Body:
-            {result.request.body}
+            cookies_dict = {cookie.name: cookie.value for cookie in result.cookies}
+            allure.attach(
+                body=json.dumps(cookies_dict, indent=4),
+                name='Cookies',
+                attachment_type=AttachmentType.JSON
+            )
 
-            ==== HTTP Response ====
-            Status Code: {result.status_code}
-            Body:
-            {json.dumps(json.loads(result.text), indent=4, ensure_ascii=False) if result.text else "None"}
+            # Console log
+            log_message = f'''
+                ==== HTTP Request ====
+                Method: {result.request.method}
+                URL: {result.request.url}
+                Body:
+                {result.request.body}
 
-            ==== Cookies ====
-            {'\n'.join([f'{c.name} = {c.value}' for c in result.cookies])}
+                ==== HTTP Response ====
+                Status Code: {result.status_code}
+                Body:
+                {json.dumps(json.loads(result.text), indent=4, ensure_ascii=False) if result.text else "None"}
+
+                ==== Cookies ====
+                {'\n'.join([f'{c.name} = {c.value}' for c in result.cookies])}
             '''
+            logging.info(log_message)
 
-    logging.info(log_message)
+            return result
+    return wrapper
